13497d097090d46f96712cd9f94cfd2016d99e0e
Ahmed Akram <eng.akramovic@gmail.com>
2022-05-12 03:10:39 +0200

    Create LongestSubstringWithoutRepeatingCharacters_Medium.java

---
+++ src/LongestSubstringWithoutRepeatingCharacters_Medium.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters_Medium {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> chars = new HashMap<>();
+        int windowStart = 0;
+        int max = Integer.MIN_VALUE;
+        for(int windowEnd = 0; windowEnd < s.length(); windowEnd++){
+            char rightChar = s.charAt(windowEnd);
+            while(chars.containsKey(rightChar)){
+                char leftChar = s.charAt(windowStart);
+                chars.put(leftChar, chars.get(leftChar) - 1);
+                if(chars.get(leftChar) == 0)
+                    chars.remove(leftChar);
+                windowStart++;
+            }
+            chars.put(rightChar ,chars.getOrDefault(rightChar, 0) + 1);
+            max = Math.max(max, windowEnd - windowStart + 1);
+        }
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
+}
