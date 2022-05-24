57ca64b16840f3d54e2af83f8f39b73258bbbe22
Ahmed Akram <eng.akramovic@gmail.com>
2022-05-24 04:30:24 +0200

    Create FirstMissingPositive_Hard.java

---
+++ src/FirstMissingPositive_Hard.java
@@ -0,0 +1,22 @@
+public class FirstMissingPositive_Hard {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            int j = nums[i] - 1;
+            if(nums[i] < nums.length && nums[i] > 0 &&nums[i] != nums[j])
+                swap(nums,i,j);
+            else
+                i++;
+        }
+        for(i = 0; i < nums.length; i++){
+            if(nums[i] != i + 1)
+                return i + 1;
+        }
+        return nums.length + 1;
+    }
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
