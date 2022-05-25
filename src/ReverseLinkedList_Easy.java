142ffb1ac6771ec19386d468d63565fb54d5743f
Ahmed Akram <eng.akramovic@gmail.com>
2022-05-25 09:01:16 +0200

    Create ReverseLinkedList_Easy.java

---
+++ src/ReverseLinkedList_Easy.java
@@ -0,0 +1,12 @@
+public class ReverseLinkedList_Easy {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, next = null;
+        while(head != null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
